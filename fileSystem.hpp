833c06d53cfa9e6572dcea961b1df1ba786793a7
araihan1 <araihan1@binghamton.edu>
2017-04-26 17:15:05 -0400

    Changed vectors to arrays

--- fileSystem.hpp
+++ fileSystem.hpp
@@ -1,6 +1,6 @@
 //
 //  fileSystem.hpp
-//  
+//
 //
 //  Created by oliver on 4/24/17.
 //
@@ -17,29 +17,29 @@
 #include "iNode.hpp"
 
 class fileSystem{
-
-
-	public:
-		//Constructors
-		fileSystem(std::string diskName);
-		std::vector<iNode> iNodeList;
-	
-	
-		void create(std::string ssfsFName);
-		void import(std::string ssfsFName, std::string unixFName);
-		void cat(std::string ssfsFName);
-		void del(std::string ssfsFName);
-		void write(std::string ssfsFName, char ch, int startByte, int numBytes);
-		void read(std::string ssfsFName, int startByte, int numBytes);
-		std::string list();
-		void shutdown();
-	
-	private:
-		std::vector<bool> freeBlockList;
-		std::vector<bool> freeiNodeList;	
-		std::vector<iNode> iNodeList;
-	
-	
+    
+    
+public:
+    //Constructors
+    fileSystem(std::string diskName);
+    std::vector<iNode> iNodeList;
+    
+    
+    void create(std::string ssfsFName);
+    void import(std::string ssfsFName, std::string unixFName);
+    void cat(std::string ssfsFName);
+    void del(std::string ssfsFName);
+    void write(std::string ssfsFName, char ch, int startByte, int numBytes);
+    void read(std::string ssfsFName, int startByte, int numBytes);
+    std::string list();
+    void shutdown();
+    
+private:
+    bool * freeBlockList; // allocate this when we know size
+    bool freeiNodeList [256] = false;
+    iNode iNodeList [256];
+    
+    
 };
 
 
